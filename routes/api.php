9bdd3afabb21f3b069b8013a916aa2937a97277e
Kettei <97405768+KetteiPrOt@users.noreply.github.com>
2024-09-19 12:54:41 -0500

    Implemented simple Login test endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\Login;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login', [Login::class, 'login']);
