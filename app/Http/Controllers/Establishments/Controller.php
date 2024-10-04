d883ba63786acfb29355285296981a614214a3df
Kettei <97405768+KetteiPrOt@users.noreply.github.com>
2024-10-04 18:26:23 -0500

    Added Establishment update, show, and destroy endpoints

--- app/Http/Controllers/Establishments/Controller.php
+++ app/Http/Controllers/Establishments/Controller.php
@@ -6,16 +6,17 @@ use App\Http\Resources\Establishments\Resource;
 use App\Models\Establishments\Model as Establishment;
 use App\Http\Controllers\Controller as BaseController;
 use App\Http\Requests\Establishments\StoreRequest;
+use App\Http\Requests\Establishments\UpdateRequest;
 use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController
 {
     public function index()
     {
-        $estabishments = Establishment::where(
+        $establishments = Establishment::where(
             'user_id', Auth::user()->id
         )->orderBy('code', 'asc')->get();
-        return Resource::collection($estabishments);
+        return Resource::collection($establishments);
     }
 
     public function store(StoreRequest $request)
@@ -27,6 +28,28 @@ class Controller extends BaseController
         return response(['message' => 'Guardado'], 200);
     }
 
+    public function show(Establishment $establishment)
+    {
+        $this->authorizeEstablishment($establishment);
+        return new Resource($establishment);
+    }
+
+    public function update(UpdateRequest $request, Establishment $establishment)
+    {
+        $validated = $request->validated();
+        $validated['code'] = $this->numberToChar($validated['code']);
+        $validated['user_id'] = Auth::user()->id;
+        $establishment->update($validated);
+        return response(['message' => 'Actualizado.'], 200);
+    }
+
+    public function destroy(Establishment $establishment)
+    {
+        $this->authorizeEstablishment($establishment);
+        $establishment->delete();
+        return response(['message' => 'Eliminado.'], 200);
+    }
+
     private function numberToChar(int $n): string
     {
         // 100 - 999
@@ -35,5 +58,13 @@ class Controller extends BaseController
         if($n > 9) return "0$n";
         // 1 - 9
         return "00$n";
+    }
+
+    private function authorizeEstablishment(Establishment $establishment): void
+    {
+        $userEstablishments = Auth::user()->establishments;
+        if( ! $userEstablishments->contains($establishment) ){
+            abort(403, message: 'This action is unauthorized.');
+        }
     }
 }
---
+++ app/Http/Requests/Establishments/UpdateRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests\Establishments;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Rules\Unique\Code as UniqueFor;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        $establishment = $this->route('establishment');
+        return Auth::user()->establishments->contains($establishment);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $user = Auth::user();
+        return [
+            'code' => [
+                'required', 'integer', 'min:1', 'max:999',
+                new UniqueFor(
+                    $user,
+                    relation: 'establishments',
+                    ignore: $this->route('establishment')->id
+                )
+            ],
+            'address' => 'required|string|max:255',
+            'commercial_name' => 'required|string|max:255',
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'code' => 'código',
+            'address' => 'dirección',
+            'commercial_name' => 'nombre comercial'
+        ];
+    }
+}
