233a7c0c46f9ac9de74564d008f281d484710d1c
Kettei <97405768+KetteiPrOt@users.noreply.github.com>
2024-10-11 10:10:21 -0500

    Improved IdentificationType migration

--- database/migrations/2024_10_09_024145_create_persons_table.php
+++ database/migrations/2024_10_09_024145_create_persons_table.php
@@ -13,15 +13,23 @@ return new class extends Migration
     {
         Schema::create('persons', function (Blueprint $table) {
             $table->id();
-            $table->string('identification',13);
-            $table->string('social_reason',255);
-            $table->string('email',255);
-            $table->string('phone_number',10)->nullable();
+            $table->string('identification', 13);
+            $table->string('social_reason', 255);
+            $table->string('email', 255);
+            $table->string('phone_number', 15)->nullable();
             $table->string('address', 255)->nullable();
             $table->timestamps();
-            $table->foreignId('user_id')->constrained();
+            // Foreign Keys
             $table->unsignedTinyInteger('identification_type_id');
-            $table->foreign('identification_type_id')->references('id')->on('identification_types');
+            $table->foreign('identification_type_id')
+                  ->references('id')
+                  ->on('identification_types')
+                  ->onUpdate('restrict')
+                  ->onDelete('restrict');
+            $table->foreignId('user_id')
+                  ->constrained()
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
         });
     }
 
